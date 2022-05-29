43de1e80e36b39006597e82e635f15c0b2297321
diogovasconcelos99 <diogovasc99@gmail.com>
2022-05-29 22:39:18 +0100

    dsaasd

--- src/sdstore.c
+++ src/sdstore.c
@@ -1,60 +1,73 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-typedef struct command {
-    char *tipo;
-    int n_args;
-    char* args;
-} *Command;
-
-typedef struct cmdlist {
-    Command* cmds;
-    int no_cmd;
-} *CmdList;
 
 
-void print_commands (CmdList cl) {
-    for(int i=0; i<cl->no_cmd; i++) {
-        char w[128];
-        Command c = cl->cmds[i];
 
-        
+int main(int argc, char* argv[]) {
 
-        write(1, "w", strlen(w));
+    if(argc == 1) {
+        write(1, "./sdstore status\n", strlen("./sdstore status\n"));
+        write(1, "./sdstore proc-file input-filename output-filename transformation-id-1 transformation-id-2 ...\n", 
+        strlen("./sdstore proc-file input-filename output-filename transformation-id-1 transformation-id-2 ...\n"));
+
+        return 1;
     }
-}
-
-
-
-
-
-
-
-int main(int argc, const char* argv[]) {
 
     int fifo_c2s = open("tmp/fifo_c2s", O_WRONLY);
     if(fifo_c2s == -1) {
         perror("fifo_c2s");
     }
 
-    if(argc == 1) {
-
+    if(strcmp(argv[1], "status") != 0 && strcmp(argv[1], "proc-file") != 0) {
+        perror("só podem ser executados os comandos 'status' e 'proc-file'");
     }
 
-    char line[128] = "";
-    int i;
-    for(i=1; i < argc-1; i++) {
-        strcat(line, argv[i]);
-        strcat(line, " ");
+    if(strcmp("status", argv[1]) == 0 && argc != 2) {
+        perror("erro no número de argumentos");
+
+        return -1;
     }
-    strcat(line, argv[i]);
+    if(strcmp("proc-file", argv[1]) == 0 && argc < 5) {
+        perror("erro no número de argumentos");
 
+        return -1;
+    }
 
-    write(1, line, strlen(line));
-    write(fifo_c2s, line, strlen(line));
+    int pid = getpid();
+    char w[256];
+    sprintf(w, "%d ", pid);
+
+    strcat(w, argv[1]);
+    strcat(w, " ");
     
+    for(int i=2; i<argc-1; i++) {
+        strcat(w, argv[i]);
+        strcat(w, " ");
+    }
+    strcat(w, argv[argc-1]);
+
+    strcat(w, "\n");
+
+    if(strcmp(argv[1], "proc-file") == 0) {
+        int in_fd = open(argv[2], O_RDONLY);
+        int out_fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+        if(in_fd < 0 || out_fd < 0) {
+            perror("abertura de um ou dos ficheiros a processar");
+
+            return -1;
+        }
+    }
+
+    // write(1, w, strlen(w));
+    write(fifo_c2s, w, strlen(w));
+
+    char pipe[16];
+    sprintf(pipe, "tmp/pipe_%d", pid);
 
     return 0;
 }
--- src/sdstored.c
+++ src/sdstored.c
@@ -6,11 +6,37 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#include "../include/sdstored.h"
-
 
 #define NO_TRNSF 7
 #define BUFSIZE 1024
+
+
+typedef struct request {
+    int client_pid;
+    char* tipo;
+    char* input_file;
+    char* output_file;
+    int no_trnsf;
+    char* trnsfs[32];
+} *Request;
+
+
+typedef struct queue {
+    Request* request;
+    struct queue* prox;
+} Queue;
+
+
+
+typedef struct transform {
+    char *tipo;
+    int capacidade;
+    int disp;
+} *Transform;
+
+
+
+
 
 int readln(int fd, char *line, size_t size) {
 	int bytes_read;
@@ -81,11 +107,107 @@ void free_transforms(Transform *t) {
 }
 
 
+Request proc_request(char* line) {
+        Request r = malloc(sizeof(struct request));
+
+        char *aux[32], *aux1, *aux2, *aux3, *aux4, *aux5, *token;
+        
+        token = strtok(line, " ");
+        aux4 = strdup(token);
+        r->client_pid = atoi(aux4);
+        r->no_trnsf = 0;
+
+        
+        
+        token = strtok(NULL, " ");
+        aux1 = strdup(token);
+        r->tipo = aux1;
+
+        
+        if(strcmp(r->tipo, "status") == 0) {
+            return r;
+        }
+
+        if(strcmp(r->tipo, "proc-file") == 0) {
+           
+            token = strtok(NULL, " ");
+            aux2 = strdup(token);
+            r->input_file = aux2;
+
+        
+        
+            token = strtok(NULL, " ");
+            aux3 = strdup(token);
+            r->output_file = aux3;
+
+            token = strtok(NULL, " ");
+
+            int i=0;
+            while(token != NULL) {
+                aux5 = strdup(token);
+                // printf("%s %d ", aux, i);
+
+                token = strtok(NULL, " ");
+                aux[i] = aux5;
+            
+                i++;
+                // r->no_trnsf = i+1;
+                // printf("%s %d ", r->trnsfs[i], r->no_trnsf);
+            }
+            // printf("test");
+
+            return r;
+        }
+}
+
+
+void print_req(Request r) {
+
+    char w[256];
+    sprintf(w, "%d %s ", r->client_pid, r->tipo);
+    
+    if(strcmp(r->tipo, "status") == 0) {
+        strcat(w, "\n");
+        write(1, w, strlen(w));
+        return ;
+    }
+    if(strcmp(r->tipo, "proc-file") == 0) {
+
+        strcat(w, r->input_file);
+        strcat(w, " ");
+
+        strcat(w, r->output_file);
+        strcat(w, " ");
+
+        
+        for(int i=0; i<r->no_trnsf; i++) {
+            strcat(w, r->trnsfs[i]);
+            strcat(w, " ");
+        }
+        /*
+        strcat(w, atoi(r->no_trnsf));
+        strcat(w, " ");
+        */
+
+        strcat(w, "\n");
+        write(1, w, strlen(w));
+        return ;
+    }
+}
+
+void print_req2 (Request r) {
+    printf("%d %s %s", r->client_pid, r->input_file, r->output_file);
+}
+
+
+
 void init_queue (Request* request){
     Queue* queue = malloc(sizeof(struct queue));
     queue->request = request;
     queue->prox = NULL;
 }
+
+
 
 void adicionarPedidoQueue(Queue** pedidos, Request* request){
     if (pedidos == NULL) return;
@@ -96,10 +218,17 @@ void adicionarPedidoQueue(Queue** pedidos, Request* request){
 }
 
 
-void exec_req (char* trnsf_dir, char* tipo, char* in_pathfile, char* out_pathfile) {
 
-    int in_fd = open(in_pathfile, O_RDONLY);
-    int out_fd = open(out_pathfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+
+void exec_req (char* trnsf_dir, Request r) {
+
+    int in_fd = open(r->input_file, O_RDONLY);
+    int out_fd = open(r->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if(in_fd < 0 || out_fd < 0) {
+        perror("abertura de um ou dos ficheiros a processar");
+    }
 
     if (in_fd < 0 || out_fd < 0) {
         perror("erro na pathfile do ficheiro a processar ou processado");
@@ -107,7 +236,7 @@ void exec_req (char* trnsf_dir, char* tipo, char* in_pathfile, char* out_pathfil
     }
 
     char* cmd = strdup(trnsf_dir);
-    strcat(cmd, tipo);
+    strcat(cmd, r->trnsfs[0]);
     // write(1, search, strlen(search));
 
     int fd[2];
@@ -154,7 +283,7 @@ int main(int argc, char* argv[]) {
     
     show_transforms(trnsf);
     
-    // exec_req(transform_path, "nop", "samples/sample-1-so.m4a", "outputs/sample-2-so.m4a");
+    
 
 
     
@@ -165,7 +294,19 @@ int main(int argc, char* argv[]) {
     char buf[BUFSIZE];
     while(1) {
         if((bytes_read = readln(fifo_c2s, buf, BUFSIZE)) > 0) {
-            write(1, buf, strlen(buf));
+            // write(1, buf, strlen(buf));
+
+            Request req = malloc(sizeof(struct request));
+            char* to_proc = strdup(buf);
+
+            req = proc_request(to_proc);
+
+            print_req2(req);
+
+            // exec_req(transform_path, req);
+
+            char pipe[16];
+            sprintf(pipe, "tmp/pipe_%d", req->client_pid);
         }
     }
     
